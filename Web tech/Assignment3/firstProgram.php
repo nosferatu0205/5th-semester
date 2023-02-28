11970fa80a019fa9051b6a5e1700ba103175936b
Mohammad Momenuzzaman <75083550+nosferatu0205@users.noreply.github.com>
2023-03-01 00:32:06 +0600

    php assignment, not complete

---
+++ Web tech/Assignment3/firstProgram.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+            <h1>First Program
+            <h1>
+    <head>
+
+    <body>
+        <?php
+        print "Running first Program with XAMPP"
+        ?>
+    </body>
+</html>
